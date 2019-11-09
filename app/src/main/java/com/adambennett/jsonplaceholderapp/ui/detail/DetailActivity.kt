8e498d2280d57f077ce1b049f30a049b18977a14
Adam Bennett <abennett.kva@gmail.com>
2019-11-09 11:21:23 +0000

    Fix ktlint issues

--- app/src/main/java/com/adambennett/jsonplaceholderapp/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/adambennett/jsonplaceholderapp/ui/detail/DetailActivity.kt
@@ -96,8 +96,12 @@ fun DefaultPreview() {
         Surface(color = +themeColor { surface }) {
             Detail(
                 ListDisplayModel(
-                    title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-                    body = "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+                    title = "sunt aut facere repellat provident occaecati excepturi optio" +
+                        " reprehenderit",
+                    body = "quia et suscipit\n" +
+                        "suscipit recusandae consequuntur expedita et cum\n" +
+                        "reprehenderit molestiae ut ut quas totam\n" +
+                        "nostrum rerum est autem sunt rem eveniet architecto",
                     commentCount = 4,
                     username = "Bret"
                 )
